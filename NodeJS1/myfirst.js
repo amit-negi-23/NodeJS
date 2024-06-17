20b4db92295f9acbb01d4f3c92776d1df5e39baf
amit-negi-23 <amitnegiit23@gmail.com>
2024-06-17 13:06:56 +0530

    res.write()

--- NodeJS1/myfirst.js
+++ NodeJS1/myfirst.js
@@ -2,7 +2,8 @@ let http = require("http");
 let user = require("./myfirstmodule")
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end("<h1>Hello World!</h1>");
+    res.write("Today is "+ user.myDateTime())
+    res.end();
     console.log("Server started")
 }).listen(8080);
 
