b9c722ab3e21fc397e6cf8ebb8e162470e1a5ec1
amit-negi-23 <amitnegiit23@gmail.com>
2024-06-17 13:18:53 +0530

    url module

--- NodeJS1/myfirst.js
+++ NodeJS1/myfirst.js
@@ -1,8 +1,12 @@
 let http = require("http");
+let url  = require("url");
+
 let user = require("./myfirstmodule")
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url)
+    let q= url.parse(req.url, true).query;
+    let text = q.name + " - " + q.email
+    res.write(text);
     res.end();
     console.log("Server receive request")
 }).listen(8080);
