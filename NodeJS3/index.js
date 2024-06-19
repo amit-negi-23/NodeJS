8f587ffbcc7fa13e367ed1b229f0e332207a22cc
amit-negi-23 <amitnegiit23@gmail.com>
2024-06-19 11:32:06 +0530

    url.parse(req.url)

--- NodeJS3/index.js
+++ NodeJS3/index.js
@@ -1,8 +1,19 @@
+let http = require("http");
 let url = require("url");
-let adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+let fs = require("fs");
 
+http.createServer(function (req, res) {
+    let q = url.parse(req.url, true);
+    let filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
 
-let q = url.parse(adr, true);
-
-let qdata = q.query;
-console.log(qdata);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
